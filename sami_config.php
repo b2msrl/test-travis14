a5038f1e2050cf83e822a7688365ff3f65c0c855
alevento <a@a.it>
2016-10-20 17:47:34 +0200

    Substitute

--- sami_config.php
+++ sami_config.php
@@ -19,10 +19,10 @@ $versions = GitVersionCollection::create($dir)
 
 return new Sami($iterator,array(
     'theme'                => 'default',
-    'title'                => '@@@package_name API',
+    'title'                => 'test-travis14 API',
     'versions'             => $versions,
-    'build_dir'            => '@@@doc_destination/build/%version%',
-    'cache_dir'            => '@@@doc_destination/cache/%version%',
+    'build_dir'            => 'Y:/Public/laravel-packages/www/doc/b2msrl/test-travis14/build/%version%',
+    'cache_dir'            => 'Y:/Public/laravel-packages/www/doc/b2msrl/test-travis14/cache/%version%',
     'default_opened_level' => 2,
 ));
 
--- tests/config/sedCommandProvider.sh
+++ tests/config/sedCommandProvider.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i "s/'providers' => \[/'providers' => \[ @@@namespacevendor\\\\@@@namespacepackage_name\\\\@@@providerpackage_nameServiceProvider::class,/g" ./config/app.php
+sed -i "s/'providers' => \[/'providers' => \[ Padosoft\\\\Testtravis14\\\\Testtravis14ServiceProvider::class,/g" ./config/app.php
